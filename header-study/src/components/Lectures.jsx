69eb5ae50ca0c1a77c88e3114efd3002eedf80c2
sinjwu <harbirdouk@gmail.com>
2025-08-04 14:10:21 +0900

    250804sc

--- header-study/src/components/Lectures.jsx
+++ header-study/src/components/Lectures.jsx
@@ -3,7 +3,7 @@ import lectureData from "../lectureData.json";
 
 const Lectures = () => {
   return (
-    <ul className="bg-blue-100 grid grid-cols-4 max-w-7xl mx-auto justify-items-center mt-10">
+    <ul className="bg-blue-100 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 max-w-7xl mx-auto justify-items-center mt-10">
       {lectureData.map((lectureData, index) => (
         <LectureCard
           key={index}
